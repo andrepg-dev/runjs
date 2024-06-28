702c89771c90e7b9f01f71e9fb9764ac78365e19
Andre <89555120+AndrePonce3322@users.noreply.github.com>
2024-06-28 07:20:37 -0600

    feat: Update page metadata and add favicon and manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,20 @@ import Head from 'next/head'
 export const metadata: Metadata = {
   title: 'NinjaJS - Online JavaScript Editor',
   description: 'NinjaJS is an online JavaScript editor that allows you to write, run, and share your code in real-time.',
+  assets: ["favicon.ico"],
+  applicationName: 'NinjaJS',
+  creator: 'NinjaJS',
+  twitter: {
+    card: 'summary',
+    site: '@ninja_js',
+    creator: '@ninja_js',
+    images: ['/android-chrome-512x512.png'],
+  },
+  authors: {
+    name: 'Andre Ponce',
+    url: 'https://andrepg.me'
+  },
+  keywords: ['ninja', 'js', 'javascript', 'editor', 'code', 'online', 'real-time'],
 }
 
 export default function RootLayout({
